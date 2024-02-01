21d42f5b715451675d60e624b99d355f6b8d8b5d
Jakub Stapiński <jaksta65@gmail.com>
2024-02-01 17:48:46 +0100

    bugfixes

--- src/Pages/RemakeStudio.tsx
+++
@@ -1,9 +0,0 @@
-import React from 'react'
-
-const RemakeStudio = () => {
-  return (
-    <div>RemakeStudio</div>
-  )
-}
-
-export default RemakeStudio
--- src/main.tsx
+++ src/main.tsx
@@ -1,4 +1,4 @@
-import { ReactNode, StrictMode, Suspense } from 'react'
+import { ReactNode, StrictMode } from 'react'
 import ReactDOM from 'react-dom/client'
 import './index.css'
 import { BrowserRouter, Route, Routes } from 'react-router-dom'
